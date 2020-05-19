a50346f54648932da27c10dd9576bd91d6c9493a
Marcelina Bałamut <marcelina.b@interia.pl>
2020-05-19 21:11:39 +0200

    Add implementation for potential algorithms

---
+++ src/sample/CumulativeMovingAverage.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.List;
+
+public class CumulativeMovingAverage {
+    int n = 0;
+    double average = 0.0;
+
+    public List<Double> getCMA(List<Double> data){
+        for (int i=0;i<data.size();i++) {
+            data.set(i,add(data.get(i)));
+        }
+        return data;
+    }
+
+    public double add(double x) {
+        return average += (x - average) / ++n;
+    }
+
+}
---
+++ src/sample/ExponentialMovingAverage.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.util.List;
+
+public class ExponentialMovingAverage {
+    private double alpha;
+    private Double oldValue;
+
+    public ExponentialMovingAverage(double alpha) {
+        this.alpha = alpha;
+    }
+
+    public List<Double> getEMA(List<Double> data){
+        //List<Double> ema_data = new ArrayList<>(data.size());
+
+        for(int i=0;i<data.size();++i) {
+            data.set(i,average(data.get(i)));
+        }
+        return data;
+    }
+
+    public double average(double value) {
+        if (oldValue == null) {
+            oldValue = value;
+            return value;
+        }
+        double newValue = oldValue + alpha * (value - oldValue);
+        oldValue = newValue;
+        return newValue;
+    }
+}
